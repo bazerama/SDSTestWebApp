13ae4faeb347ef15912b7a48090869bd8cf5db99
Sebastian Vaughan <admin@svaughan.co>
2021-09-01 20:10:27 +1000

    new stuff

--- SDSTestWebApp/Models/ErrorViewModel.cs
+++ SDSTestWebApp/Models/ErrorViewModel.cs
@@ -5,6 +5,7 @@ namespace SDSTestWebApp.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        // some change
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
